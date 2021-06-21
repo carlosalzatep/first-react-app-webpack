9ab0de1fdcbc6c440649857e3658371735cfc62a
Carlos Alzate <carlosalzatep@gmail.com>
2021-06-21 12:38:08 -0500

    Components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
+import Footer from '../components/Footer'
+
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss'
 
+const API = 'http://localhost:3000/initalState'
+
 const App = () => {
+
+    const videos = useInitialState(API)
+
     return (
         <div className="App">
             <Header/>
             <Search/>
-            <Categories>
-                <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                </Carousel>
-            </Categories>
+
+            {
+                Object.keys(videos).map( list => {
+
+                    return videos[list].length > 0 &&
+                    <Categories title={list}>
+                        <Carousel>
+                            {
+                                videos[list].map(video => 
+                                    <CarouselItem key={video.id} {...video} />
+                                )
+                            }
+                        </Carousel>
+                    </Categories>
+                })
+            }
+           
+            <Footer/>
         </div>
     )
 }
